cbd17e73f5f35a61323acfe4ec3890094e5c2115
Kishan Parmar <Kishan.Parmar@thegatewaycorp.co.in>
2024-12-12 12:38:17 +0530

    fix responsiveness for header, slight changes in inputbox

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,13 +3,13 @@ import SearchInput from "./SearchInput";
 
 export default function Header() {
   return (
-    <div className="flex items-center justify-between px-4 border-b h-14">
+    <div className="flex flex-col md:flex-row items-center justify-between px-4 border-b h-20">
       <div className="flex items-center space-x-2 text-sm">
         <Link to="/" className="text-lg font-bold">
           NPM Registry
         </Link>
       </div>
-      <div className="w-full max-w-xl ml-4">
+      <div className="w-2/3 lg:w-full max-w-xl ml-4">
         <SearchInput />
       </div>
     </div>
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -15,13 +15,13 @@ export default function SearchInput() {
   return (
     <form onSubmit={handleSubmit}>
       <div className="relative">
-        <div className="absolute inset-y-0 flex items-center pl-3">
+        <div className="absolute inset-y-0 flex items-center pl-0 md:pl-3">
           <VscSearch className="h-5 v-5 text-gray-500" />
         </div>
         <input
           value={term}
           onChange={(e) => setTerm(e.target.value)}
-          className="pl-10 py-2 w-full border-0 shadow-none"
+          className="pl-10 py-2 w-full border-0 shadow-none focus:outline-none"
           placeholder="Search packages..."
         />
       </div>
